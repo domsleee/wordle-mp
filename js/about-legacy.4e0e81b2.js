d227f2b8b8b226e1ec9c96976e32f5bbd919c85c
Dom Slee <domslee1@gmail.com>
2022-03-19 17:01:34 +1100

    gh-pages

---
+++ js/about-legacy.4e0e81b2.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwordle_mp"]=self["webpackChunkwordle_mp"]||[]).push([[443],{5087:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],r=n(1001),s={},a=(0,r.Z)(s,u,l,!1,null,null,null),c=a.exports}}]);
+//# sourceMappingURL=about-legacy.4e0e81b2.js.map
